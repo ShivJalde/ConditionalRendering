52b0cff425e68c3f51e3b6a5953d2a476dd7339e
Shivraj <shivraj902177@gmail.com>
2023-10-03 19:42:25 +0530

    code destructerd properly

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Product.css";
 import { useState } from "react";
-import axios from "axios";
 import Sidebar from "../Sidebar/Sidebar";
 import Cards from "../Cards/Cards";
 import NOData from "../NoData/NOData";
@@ -9,20 +8,12 @@ import NOData from "../NoData/NOData";
 function Products() {
   const [allProductsData, setAllProductsData] = useState([]);
 
-  // const fetchalldata = () => {
-  //   axios
-  //     .get("https://fakestoreapi.com/products")
-  //     .then((res) => {
-  //       setAllProductsData(res.data);
-  //     })
-  //     .catch((err) => {
-  //       alert("something went wrong");
-  //       console.log(err);
-  //     });
-  // };
   return (
     <div className="product-main-container">
-      <Sidebar setAllProductData={setAllProductsData} fetchalldata={setAllProductsData} />
+      <Sidebar
+        setAllProductData={setAllProductsData}
+        fetchalldata={setAllProductsData}
+      />
       <div></div>
 
       {allProductsData.length > 0 ? (
